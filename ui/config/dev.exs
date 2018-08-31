e1d370e5f446fd4f9914966364af662194c43e04
Andreas Altendorfer <andreas@altendorfer.at>
2018-08-31 12:31:58 +0200

    Config: 1s delay in dev-mode

--- ui/config/dev.exs
+++ ui/config/dev.exs
@@ -11,8 +11,15 @@ config :ui, UiWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  measure_interval: 1_000,
+  watchers: [
+    node: [
+      "node_modules/brunch/bin/brunch",
+      "watch",
+      "--stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # ## SSL Support
 #
